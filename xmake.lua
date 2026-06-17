19e9570427f883e37c24578c73835f5e9d16e1ff
needherb <228988290+needherb@users.noreply.github.com>
2026-06-17 04:29:19 -0500

    Bump version to 0.4.0 and update build rules

--- xmake.lua
+++ xmake.lua
@@ -3,13 +3,13 @@ includes("lib/commonlibob64")
 
 -- set project constants
 set_project("LootGlow")
-set_version("0.3.2")
+set_version("0.4.0")
 set_license("GPL-3.0")
 set_languages("c++23")
 set_warnings("allextra")
 
 -- add common rules
-add_rules("mode.debug", "mode.releasedbg")
+add_rules("mode.debug", "mode.release", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
 -- define targets
@@ -25,3 +25,10 @@ target("LootGlow")
     add_headerfiles("src/**.h")
     add_includedirs("src")
     set_pcxxheader("src/pch.h")
+
+    if is_mode("release") then
+        set_symbols("none")
+        set_optimize("smallest")
+        add_cxflags("/O1", "/Gy", "/Gw", { force = true })
+        add_ldflags("/OPT:REF", "/OPT:ICF", "/DEBUG:NONE", { force = true })
+    end
